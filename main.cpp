091af6c54158b128242f03e97572c7a24bb7ffb0
Lorenzo Tucci <lorenzo@unitoo.it>
2024-07-28 11:00:31 +0300

    Update plugin init

--- main.cpp
+++ main.cpp
@@ -22,10 +22,7 @@ static void onNewWindow(void* self, std::any data) {
 APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
     PHANDLE = handle;
 
-    g_pGlobalState = std::make_unique<SGlobalState>();
-
     static auto P  = HyprlandAPI::registerCallbackDynamic(PHANDLE, "openWindow", [&](void* self, SCallbackInfo& info, std::any data) { onNewWindow(self, data); });
-                                                          [&](void* self, SCallbackInfo& info, std::any data) { onUpdateWindowRules(std::any_cast<PHLWINDOW>(data)); });
 
     HyprlandAPI::reloadConfig();
 
