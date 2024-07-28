1303cf4e9489b2de4e310e3c80f34fc9b28b6626
Lorenzo Tucci <lorenzo@unitoo.it>
2024-07-28 10:54:49 +0300

    Updating plugin init

--- main.cpp
+++ main.cpp
@@ -18,3 +18,18 @@ static void onNewWindow(void* self, std::any data) {
     );
 
 }
+
+APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
+    PHANDLE = handle;
+
+    g_pGlobalState = std::make_unique<SGlobalState>();
+
+    static auto P  = HyprlandAPI::registerCallbackDynamic(PHANDLE, "openWindow", [&](void* self, SCallbackInfo& info, std::any data) { onNewWindow(self, data); });
+                                                          [&](void* self, SCallbackInfo& info, std::any data) { onUpdateWindowRules(std::any_cast<PHLWINDOW>(data)); });
+
+    HyprlandAPI::reloadConfig();
+
+    HyprlandAPI::addNotification(PHANDLE, "[kuukiyomu] Initialized successfully!", CColor{0.2, 1.0, 0.2, 1.0}, 5000);
+
+    return {"kuukiyomu", "A smooth hacky session manager plugin", "yamabiiko", "0.1"};
+}
