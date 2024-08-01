3ee221e1c3d6a1fa6fadda3f0680cc3f106e0b13
Lorenzo Tucci <lorenzo@unitoo.it>
2024-08-01 23:12:38 +0300

    Add custom notification

--- main.cpp
+++ main.cpp
@@ -65,8 +65,11 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
     auto g_pSessionData = std::make_unique<SessionData>();
     bool save = HyprlandAPI::addDispatcher(PHANDLE, "kuukiyomu:save", saveSession);
     bool load = HyprlandAPI::addDispatcher(PHANDLE, "kuukiyomu:load", loadSession);
-    if(save && load) {
-    	HyprlandAPI::addNotification(PHANDLE, "[kuukiyomu] added dispatchers", CColor{0.2, 1.0, 0.2, 1.0}, 5000);
+    if(!save) {
+    	HyprlandAPI::addNotification(PHANDLE, "[kuukiyomu] could not add save dispatcher", CColor{0.2, 1.0, 0.2, 1.0}, 5000);
+    }
+    if(!load) {
+    	HyprlandAPI::addNotification(PHANDLE, "[kuukiyomu] could not add load dispatcher", CColor{0.2, 1.0, 0.2, 1.0}, 5000);
     }
 
     HyprlandAPI::reloadConfig();
@@ -75,4 +78,3 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
 
 }
 
-APICALL EXPORT void PLUGIN_EXIT() {}
